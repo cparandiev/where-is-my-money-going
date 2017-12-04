9c0c25b8d232aaec02b9bd19da2a42c9b0290d06
Tsvetko Parandiev <tsvetko.p@sbtech.com>
2017-12-04 17:17:10 +0200

    add nav bar and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap-css'
+
 
 import Routes from './Routes'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Routes />
+        <Header/>
+        <Routes/>
       </div>
     );
   }
---
+++ client/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const Header = () => (
+
+    <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+        <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <Link className="nav-link" to='/'>Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/about'>About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/contact'>Contact</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
+
+export default Header
